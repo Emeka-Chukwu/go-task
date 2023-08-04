38df1170216bc5f28a8a0b9b123e1672a5039381
emeka-chukwu <emekapascal11@gmail.com>
2023-08-04 18:47:46 +0100

    updates

--- internal/auths/repository/postgres/register.go
+++ internal/auths/repository/postgres/register.go
@@ -5,10 +5,6 @@ import (
 	resp "go-task/domain/auths/response"
 )
 
-// func (auth *authentication) register(data domain.RegisterModel) resp.RegisterResponse {
-// 	return resp.RegisterResponse{}
-// }
-
 // register implements auths.Authentication.
 func (auth *authentication) register(data domain.RegisterModel) (resp.RegisterResponse, error) {
 	panic("unimplemented")
