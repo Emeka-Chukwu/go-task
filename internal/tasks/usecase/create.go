bcd2abb435db782b8a79ad5d09bf11229e873097
emeka-chukwu <emekapascal11@gmail.com>
2023-08-09 20:17:38 +0100

    writing usecase for tasks

--- internal/tasks/usecase/create.go
+++ internal/tasks/usecase/create.go
@@ -1,6 +1,9 @@
 package tasks
 
-import domain "go-task/domain/task/request"
+import (
+	"errors"
+	domain "go-task/domain/task/request"
+)
 
 // CreateTask implements Taskusecase.
 func (t *taskusecase) CreateTask(data domain.TaskModel) (resp ResponseData, err error) {
@@ -8,6 +11,7 @@ func (t *taskusecase) CreateTask(data domain.TaskModel) (resp ResponseData, err 
 	if errdetails != nil {
 		resp.Error = errdetails
 		resp.Message = "validation error"
+		err = errors.New("validation error")
 		return
 	}
 	task, err := t.store.CreateTask(data)
@@ -16,5 +20,7 @@ func (t *taskusecase) CreateTask(data domain.TaskModel) (resp ResponseData, err 
 		resp.Message = "internal error"
 		return
 	}
-	return ResponseData{Data: task, Message: "success"}, nil
+	resp.Data = task
+	resp.Message = "success"
+	return
 }
